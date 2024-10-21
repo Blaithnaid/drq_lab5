822af30aeb54b66fad1119478845fac79319ba28
Blaithnaid <iarlaken@gmail.com>
2024-10-21 14:46:07 +0100

    comments

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const app = express();
 const port = 3000;
 
 app.use((err, req, res, next) => {
+  // if there is an error, this will catch it and send a 500 status code
   console.error(err.stack);
   res.status(500).send("Something went wrong!");
 });
 
 app.get("/", (req, res) => {
+  // if our url route is blank, this will be displayed
   res.send("Welcome to Data Representation & Querying");
 });
 
 app.get("/hello/:name/:sname", (req, res) => {
-  res.send(req.params.name);
+  // if our url route is /hello/:name, this will be displayed
+  res.send(req.params.name); // this will display the name that is inputted in the url
 });
 
 app.listen(port, () => {
